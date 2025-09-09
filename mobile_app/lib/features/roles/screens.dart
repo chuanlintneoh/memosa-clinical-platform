d57260d3a95e3be6049523489b5e4155234f41ff
chuanlintneoh <chuanlin.tneoh@gmail.com>
2025-09-10 01:52:12 +0800

    Frontend: Study coordinator create case

--- mobile_app/lib/features/roles/screens.dart
+++ mobile_app/lib/features/roles/screens.dart
@@ -1,5 +1,5 @@
 export 'admin/export_mastersheet.dart';
-export 'clinician/diagnose_cases.dart';
+export 'clinician/undiagnosed_cases.dart';
 export 'study_coordinator/draft_cases.dart';
-export 'study_coordinator/edit_cases.dart';
+export 'study_coordinator/edit_case.dart';
 export 'not_found.dart';
